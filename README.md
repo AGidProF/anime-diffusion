1b72a5b6499dd03388b820a326f8254ba8e75915
ThamerMalki <ThamerGDev@gmail.com>
2023-06-26 14:39:56 +0300

    ^_^

---
+++ README.md
@@ -0,0 +1,11 @@
+<div align="center">
+  <h1>Simple and free Text-to-Image anime-style ⛩️</h1>
+
+This project has been built with a free AI model called [anime-kawai-diffusion](https://huggingface.co/Ojimi/anime-kawai-diffusion) By Hugging Face.🤗
+
+  <h1>⚠️Usage warning⚠️</h1>
+</div>
+<ul>
+  <li>The output images may not be suitable for all ages, please use with additional safety measures. (violence, sexual) </li>
+  <li>Some parts of the model may not be suitable for you. Please use it with caution.</li>
+   </ul>
---
+++ index.js
@@ -0,0 +1,26 @@
+
+let text = document.querySelector('textarea');
+let button = document.querySelector('button');
+let img = document.querySelector('img');
+
+
+async function query(data) {
+	let response = await fetch(
+		"https://api-inference.huggingface.co/models/Ojimi/anime-kawai-diffusion",
+		{
+			headers: { Authorization: "Bearer {API_TOKEN}" },
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	let result = await response.blob();
+	return result;
+}
+
+button.addEventListener('click',()=>{
+		query({inputs: text.value}).then((response) => {
+			let objectURL = URL.createObjectURL(response);
+			img.src = objectURL;
+		});
+})
+
